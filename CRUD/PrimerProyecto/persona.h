b6b6b5cbba2fe6dad5b7ad322a3d1802e882f616
Arcangel <agustinhernandezleo@gmail.com>
2022-03-18 15:42:39 -0600

    Modify

---
+++ CRUD/PrimerProyecto/persona.h
@@ -0,0 +1,22 @@
+#include <string>
+#ifndef PERSONA_H
+#define PERSONA_H
+
+using namespace std;
+
+class persona
+{
+    public:
+        persona();
+        void menu();
+		void insertA();
+		void display();
+		void modify();
+    protected:
+
+    private:
+        string id,name,phone,college,address;
+
+};
+
+#endif // PERSONA_H
