acee429a05f6d052c101b6a9723bfadbe0dd0afa
sofia Tobar <kgomezt2@miumg.edu.gt>
2022-03-18 19:37:05 -0600

    search-creado

---
+++ CRUD/PrimerProyecto/persona.h
@@ -0,0 +1,23 @@
+#include <string>
+#ifndef PERSONA_H
+#define PERSONA_H
+
+using namespace std;
+
+class persona
+{
+    public:
+        persona();
+        void menu();
+		void insertA();
+		void display();
+		void modify();
+		void searchA();
+    protected:
+
+    private:
+        string id,name,phone,college,address;
+
+};
+
+#endif // PERSONA_H
